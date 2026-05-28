81524bbff56f392859e8cef9522c56ad6a9d8e5e
tsKmilo00 <elcamilo.torres08@gmail.com>
2026-05-28 14:38:15 -0400

    Delete Notificaciones/Notificaciones directory

--- Notificaciones/Notificaciones/src/main/java/com/SanosySalvos/Notificaciones/dto/NotificacionSimpleRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.SanosySalvos.Notificaciones.dto;
-
-import lombok.Data;
-
-@Data
-public class NotificacionSimpleRequestDTO {
-    private Long usuarioId;
-    private String mensaje;
-}
