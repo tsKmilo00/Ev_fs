cf4e906558d76f47b0e92ca79c305e77c416c0c8
tsKmilo00 <elcamilo.torres08@gmail.com>
2026-05-28 14:38:37 -0400

    Delete Usuarios-main/Usuarios-main directory

--- Usuarios-main/Usuarios-main/src/main/java/com/SanosySalvos/Usuarios/dto/LoginRequestDTO.java
+++
@@ -1,9 +0,0 @@
-package com.SanosySalvos.Usuarios.dto;
-
-import lombok.Data;
-
-@Data
-public class LoginRequestDTO {
-    private String correoElectronico;
-    private String contrasena;
-}
--- Usuarios-main/Usuarios-main/src/main/java/com/SanosySalvos/Usuarios/model/RolUsuario.java
+++
@@ -1,9 +0,0 @@
-package com.SanosySalvos.Usuarios.model;
-
-public enum RolUsuario {
-CIUDADANO,
-VETERINARIA,
-REFUGIO,
-MUNICIPALIDAD,
-ADMINISTRADOR
-}
